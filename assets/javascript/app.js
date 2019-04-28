e595eced405735eaff935a6015a8ae59e46309a4
unknown <you@example.com>
2019-04-27 20:35:38 -0700

    made sure my timeer stops at 0 and values are displayed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,84 +1,60 @@
 
-        var Answers = new Array();
+var Answers = new Array();
 
-        var timeLeft = 60;
-        var correctAnswers = 0;
-        var unansweredQuestions = 0;
+var timeLeft = 60;
+var correctAnswers = 0;
+var unansweredQuestions = 0;
 
+var i = 0;
+var t = "X";
 
-        var i = 0;
+var runrun;
 
+function loseTime() {
 
-        var t = "X";
+    timeLeft--;
 
-        var runrun;//actually use this to 'hold' our re-running thingy
+    document.getElementById("displayTimeLeft").innerHTML = timeLeft;
 
+    if (timeLeft < 1) { gameOver(); }
+}
 
+function gameLoad() {
 
-        function loseTime() {
+    document.getElementById("startWrapper").style.display = "none";
+    document.getElementById("gameWrapper").style.display = "block";
 
-            timeLeft--;
+    runrun = setInterval(function () { loseTime(); }, 1000);
+}
 
-            document.getElementById("displayTimeLeft").innerHTML = timeLeft;
+function gameOver() {
 
-            if (timeLeft < 1) { gameOver(); }
+    clearInterval(runrun);
+    document.getElementById("displayTimeLeft").innerHTML = "You Lose!";
 
+    Answers = document.getElementsByTagName("input");
 
-        }
-
-
-
-
-        function gameLoad() {
-
-            document.getElementById("startWrapper").style.display = "none";
-
-            document.getElementById("gameWrapper").style.display = "block";
-
-
-
-            runrun = setInterval(function () { loseTime(); }, 1000);
-
-
-        }
-
-
-        function gameOver() {
-
-            clearInterval(runrun);
-            document.getElementById("displayTimeLeft").innerHTML = "You Lose!";
-
-            Answers = document.getElementsByTagName("input");
+    if (Answers[0].checked == true) { correctAnswers++ }
+    if (Answers[3].checked == true) { correctAnswers++ }
+    if (Answers[7].checked == true) { correctAnswers++ }
+    if (Answers[9].checked == true) { correctAnswers++ }
+    if (Answers[14].checked == true) { correctAnswers++ }
+    if (Answers[16].checked == true) { correctAnswers++ }
+    if (Answers[18].checked == true) { correctAnswers++ }
+    if (Answers[22].checked == true) { correctAnswers++ }
+    if (Answers[25].checked == true) { correctAnswers++ }
+    if (Answers[29].checked == true) { correctAnswers++ }
 
 
-            if (Answers[0].checked == true) { correctAnswers++ }
-            if (Answers[3].checked == true) { correctAnswers++ }
-            if (Answers[7].checked == true) { correctAnswers++ }
-            if (Answers[9].checked == true) { correctAnswers++ }
-             if (Answers[14].checked == true) { correctAnswers++ } 
-             if (Answers[16].checked==true){correctAnswers++}
-             if (Answers[18].checked==true){correctAnswers++}
-             if (Answers[22].checked==true){correctAnswers++}
-             if (Answers[25].checked==true){correctAnswers++}
-             if (Answers[29].checked==true){correctAnswers++}
+    for (i = 0; i < Answers.length; i += 3) {
 
+        if (Answers[i].checked == false && Answers[i + 1].checked == false && Answers[i + 2].checked == false) { unansweredQuestions++ }
+    }
 
+    t = "Game Over!"
+    t += "<br><br>Unanswered: " + unansweredQuestions;
+    t += "<br><br>Correct: " + correctAnswers;
+    t += "<br><br>incorrect: " + (10 - correctAnswers - unansweredQuestions);
 
-
-
-            for (i = 0; i < Answers.length; i += 3) {
-
-                if (Answers[i].checked == false && Answers[i + 1].checked == false && Answers[i + 2].checked == false) { unansweredQuestions++ }
-
-            }
-
-
-            t = "Game Over!"
-            t += "<br><br>Unanswered: " + unansweredQuestions;
-            t += "<br><br>Correct: " + correctAnswers;
-            t += "<br><br>incorrect: " + (10 - correctAnswers - unansweredQuestions);
-
-            document.getElementById("gameWrapper").innerHTML = t;
-
-
-        }
+    document.getElementById("gameWrapper").innerHTML = t;
+}
